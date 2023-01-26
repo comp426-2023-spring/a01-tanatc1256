44bc40380d48bd8746c752b8cb191848abc00c16
root <root@PremsLaptop.localdomain>
2023-01-26 13:27:59 -0500

    webserver fixed

--- server.js
+++ server.js
@@ -27,16 +27,16 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end();
-});
+	const server = http.createServer((req, res) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('./public/index.html');
+	});
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen( 
-	port {
-	console.log('Server listening on port ${port}');
+	server.listen( 
+		port, () => {
+		console.log('Server listening on port ${port}');
 	});
 });
 // That's it! You're all done!
